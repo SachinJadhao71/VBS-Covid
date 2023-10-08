26ab3d9b1352587c2b1f18bad47dff2362985f82
sachin jadhao <jadhaosachin7171@gmail.com>
2023-10-08 17:32:15 +0530

    mail sender added

--- src/main/java/com/example/VBSCovid/Service/DoseService.java
+++ src/main/java/com/example/VBSCovid/Service/DoseService.java
@@ -6,10 +6,13 @@ import com.example.VBSCovid.Exception.Dose1AlreadyTakenException;
 import com.example.VBSCovid.Exception.Dose1NotCompletedException;
 import com.example.VBSCovid.Exception.Dose2AlreadyTakenException;
 import com.example.VBSCovid.Exception.PersonDoesNotExistException;
+import com.example.VBSCovid.Model.Certificate;
 import com.example.VBSCovid.Model.Dose;
 import com.example.VBSCovid.Model.Person;
 import com.example.VBSCovid.Repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -19,6 +22,9 @@ public class DoseService {
 
     @Autowired
     PersonRepository personRepository;
+
+    @Autowired
+    JavaMailSender javaMailSender;
     public GetDoseResponseDto getDose1(GetDoseRequestDto requestDto) {
 
         Person person = personRepository.findByEmailId(requestDto.getPersonEmail());
@@ -78,6 +84,18 @@ public class DoseService {
         person.setDose2Taken(true);
         person.getDosesTaken().add(dose);
 
+
+
+//        generating certificate after both doses are completed
+
+        Certificate certificate = new Certificate();
+        certificate.setCertificateId(String.valueOf(UUID.randomUUID()));
+        certificate.setPersonName(person.getName());
+        certificate.setPerson(person);
+        certificate.setMessage("Congratulation, Your Both Doses are completed.");
+
+        person.setCertificate(certificate);
+
         Person savedPerson = personRepository.save(person);
 
         Dose savedDose = savedPerson.getDosesTaken().get(1);
@@ -86,6 +104,32 @@ public class DoseService {
         responseDto.setPersonName(savedPerson.getName());
         responseDto.setDoseType(savedDose.getDoseType());
         responseDto.setVaccinationDate(savedDose.getVaccinationDate());
+
+//        mail sending
+
+        String text = "Hello Mr/Mrs. " +savedPerson.getName() +" \n" +
+                "First of all, congratulations on completing both of your vaccines.\n" +
+                "We have been able to vaccinate on such a large scale only because of the cooperation of many people like you.\n" +
+                "You have been vaccinated so now you have no fear of corona.\n" +
+                "Still it is our duty to care and refer those who are not yet vaccinated for. And we hope you will certainly cooperate in this.\n" +
+                "\n" +
+                "Once again congratulations...!!\n" +
+                "\n" +
+                "Your complete vaccination details are as follows:-" + "\n" +
+                "Dose 1 :- " + savedPerson.getDosesTaken().get(0).getDoseType() + "\n" +
+                "Dose 2 :- " + savedPerson.getDosesTaken().get(1).getDoseType() + "\n" ;
+
+
+
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("VaccinationBooking123@gmail.com");
+        simpleMailMessage.setTo(savedPerson.getEmailId());
+        simpleMailMessage.setSubject("Congrats.. Your Vaccination is Done..!");
+        simpleMailMessage.setText(text);
+
+        javaMailSender.send(simpleMailMessage);
+
 
 
         return responseDto;
